b90bbf37b5cd69f8432cf00565fb0710ce561fdb
Peter McGowan <pmcgowan@redhat.com>
2015-05-11 16:11:26 +0100

    Read automation.log in UTF-8 format Re-factored, and added --diff switch

--- object_walker_reader.rb
+++ object_walker_reader.rb
@@ -15,15 +15,11 @@
 # Found object_walker dump at 2014-09-17T13:28:42.052043
 # Found object_walker dump at 2014-09-17T13:34:52.649359
 # Found object_walker dump at 2014-09-17T15:06:29.250086
-# Found object_walker dump at 2014-09-17T15:22:46.034628
-# Found object_walker dump at 2014-09-18T07:56:08.201025
 # ...
 # 
 # ./object_walker_reader.rb -l -f /Documents/CloudForms/cf30-automation-log
 # Found object_walker dump at 2014-09-18T09:52:28.797868
 # Found object_walker dump at 2014-09-18T09:53:31.455892
-# Found object_walker dump at 2014-09-18T10:05:39.040744
-# Found object_walker dump at 2014-09-18T12:00:59.142460
 # ...
 # 
 # ./object_walker_reader.rb -t 2014-09-18T09:44:27.146812
@@ -38,11 +34,6 @@
 #      |    object_walker:   $evm.root['miq_server'].cpu_time = 2312.0   (type: Float)
 #      |    object_walker:   $evm.root['miq_server'].drb_uri = druby://127.0.0.1:50656   (type: String)
 #      |    object_walker:   $evm.root['miq_server'].guid = 5132a574-3d76-11e4-9150-001a4aa80204   (type: String)
-#      |    object_walker:   $evm.root['miq_server'].has_active_userinterface = true   (type: TrueClass)
-#      |    object_walker:   $evm.root['miq_server'].has_active_webservices = true   (type: TrueClass)
-#      |    object_walker:   $evm.root['miq_server'].hostname = cf31b2-1.bit63.net   (type: String)
-#      |    object_walker:   $evm.root['miq_server'].id = 1000000000001   (type: Fixnum)
-#      |    object_walker:   $evm.root['miq_server'].ipaddress = 192.168.2.77   (type: String)
 # ... 
 #
 # Author: Peter McGowan (pemcg@redhat.com)
@@ -56,95 +47,171 @@
 #               1.1-2   08-Feb-2015     Changed the regexes to search for 'objectWalker' or 'object_walker'
 #               1.1-3   09-Feb-2015     Changed all the regexes this time
 #               1.1-4   22-Mar-2015     Fixed the case where a requested timestamp that didn't exist would still dump the last object_walker output
+#               1.1-5   09-May-2015     Read automation.log in UTF-8 format
+#               1.2     11-May-2015     Re-factored, and added --diff switch
 #  
 
 require 'optparse'
+require 'tempfile'
 
 valid_timestamp_re = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}/
-object_walker_start_re = /----\] I, \[(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}) .*[Oo]bject_*[Ww]alker .* - EVM Automate Method Started/
-object_walker_body_re = /.*(AEMethod [Oo]bject_*[Ww]alker|Defined Method)\> (.*)/
-object_walker_end_re = /[Oo]bject_*[Ww]alker - EVM Automate Method Ended/
-found_timestamp = false
-dump_start = 0
+@object_walker_start_re = /----\] I, \[(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}) .*[Oo]bject_*[Ww]alker .* - EVM Automate Method Started/
+@object_walker_body_re = /.*AEMethod [Oo]bject_*[Ww]alk.*?\> (.*)/
+@object_walker_end_re = /[Oo]bject_*[Ww]alker - EVM Automate Method Ended/
 
-options = {:list => false, :filename => nil, :timestamp => nil}
+#-------------------------------------------------------------------------------------------------------------
+# Method:       list_dumps
+# Purpose:      Returns a string containing the type of the object passed as an argument
+# Arguments:    object: object to be type tested
+# Returns:      string
+#-------------------------------------------------------------------------------------------------------------
 
-parser = OptionParser.new do|opts|
-  opts.banner = "Usage: object_walker_reader.rb [options]"
-  opts.on('-l', '--list', 'list object_walker dumps in the file') do
-    options[:list] = true;
-  end
-  opts.on('-f', '--file filename', 'Full file path to automation.log (if not /var/www/miq/vmdb/log/automtion.log)') do |filename|
-    options[:filename] = filename;
-  end
-  opts.on('-t', '--timestamp timestamp', 'Date/time of the object_walker dump to be listed (hint: copy from -l output)') do |timestamp|
-    options[:timestamp] = timestamp;
-  end
-  opts.on('-h', '--help', 'Displays Help') do
-    puts opts
-    exit
-  end
-end
-parser.parse!
-
-if options[:timestamp]
-  if options[:list]
-    puts "The options -t and -l shoud not be used together: use -l to list the dumps in the log file, and -t to select a particular dump timestamp"
-    exit!
-  end
-  unless valid_timestamp_re.match(options[:timestamp])
-    puts "Invalid timestamp format"
-    exit!
-  end
-end
-  
-if options[:filename].nil?
-  filename = "/var/www/miq/vmdb/log/automation.log"
-else
-  filename = options[:filename]
-end
-
-begin
-  file = File.new(filename, "r")
-rescue => err
-  puts "Error opening log file: #{err}"
-  exit!
-end
-  
-file.each do |line|
-  match = object_walker_start_re.match(line)
-  if match
-    if options[:list]
+def list_dumps(file)
+  file.each do |line|
+    match = @object_walker_start_re.match(line)
+    if match
       puts "Found object_walker dump at #{match[1]}"
     end
-    if options[:timestamp]
-      if options[:timestamp] == match[1]
-        dump_start = file.pos - line.length
-        found_timestamp = true
-        break
-      end
-    end
-    #
-    # Default is to dump the last object_walker run
-    #
-    dump_start = file.pos - line.length
   end
 end
-if options[:timestamp] && !found_timestamp
-  puts "Timestamp: #{options[:timestamp]} not found in log file"
-  exit!
-end
 
-unless options[:list]
+#-------------------------------------------------------------------------------------------------------------
+# Method:       find_dump
+# Purpose:      Returns a string containing the type of the object passed as an argument
+# Arguments:    object: object to be type tested
+# Returns:      string
+#-------------------------------------------------------------------------------------------------------------
+
+def find_dump(file, timestamp=nil, tempfile=nil)
+  found_timestamp = false
+  dump_start = 0
+  file.each do |line|
+    match = @object_walker_start_re.match(line)
+    if match
+      unless timestamp.nil?
+        if timestamp == match[1]
+          dump_start = file.pos - line.length
+          found_timestamp = true
+          break
+        end
+      end
+      #
+      # Default is to dump the last object_walker run
+      #
+      dump_start = file.pos - line.length
+    end
+  end
+  unless timestamp.nil?
+    unless found_timestamp
+      raise "Timestamp: #{timestamp} not found in log file"
+    end
+  end
+  #
+  # Now rewind to the specified dump output and print the object_walker dump
+  #
   file.seek(dump_start)
   file.each do |line|
-    match = object_walker_body_re.match(line)
+    match = @object_walker_body_re.match(line)
     if match
-      puts match[2]
+      if tempfile.nil?
+        puts match[1]
+      else
+        tempfile.write("#{match[1]}\n")
+      end
     end
-    if object_walker_end_re.match(line)
+    if @object_walker_end_re.match(line)
       break
     end
   end
 end
-file.close
+
+#-------------------------------------------------------------------------------------------------------------
+# Method:       diff_dumps
+# Purpose:      Returns a string containing the type of the object passed as an argument
+# Arguments:    object: object to be type tested
+# Returns:      string
+#-------------------------------------------------------------------------------------------------------------
+
+def diff_dumps(file, timestamp1, timestamp2)
+  begin
+    puts "Getting diff comparison from dumps at #{timestamp1} and #{timestamp2}"
+    tempfile1 = Tempfile.new('owr-')
+    tempfile2 = Tempfile.new('owr-')
+    find_dump(file, timestamp1, tempfile1)
+    find_dump(file, timestamp2, tempfile2)
+    tempfile1.close
+    tempfile2.close
+    difference = `diff #{tempfile1.path} #{tempfile2.path}`
+    puts difference
+  ensure
+    tempfile1.unlink
+    tempfile2.unlink
+  end
+end
+
+begin
+  options = {:list => false, :filename => nil, :timestamp => nil, :diff => nil}
+  
+  parser = OptionParser.new do|opts|
+    opts.banner = "Usage: object_walker_reader.rb [options]"
+    opts.on('-l', '--list', 'list object_walker dumps in the file') do
+      options[:list] = true;
+    end
+    opts.on('-f', '--file filename', 'Full file path to automation.log (if not /var/www/miq/vmdb/log/automtion.log)') do |filename|
+      options[:filename] = filename;
+    end
+    opts.on('-t', '--timestamp timestamp', 'Date/time of the object_walker dump to be listed (hint: copy from -l output)') do |timestamp|
+      options[:timestamp] = timestamp;
+    end
+    opts.on('-d', '--diff timestamps', Array, 'Date/time of two object_walker dumps (comma separated) to be compared using \'diff\'') do |diff_timestamps|
+      unless diff_timestamps.length == 2
+        puts "timestamps must be timestamp1,timestamp2 list"
+        exit!
+      end
+      options[:diff] = diff_timestamps
+    end
+    opts.on('-h', '--help', 'Displays Help') do
+      puts opts
+      exit
+    end
+  end
+  parser.parse!
+  
+  if options[:timestamp]
+    if options[:list]
+      puts "The options -t and -l should not be used together: use -l to list the dumps in the log file, and -t to select a particular dump timestamp"
+      exit!
+    end
+    unless valid_timestamp_re.match(options[:timestamp])
+      puts "Invalid timestamp format"
+      exit!
+    end
+  end
+    
+  if options[:filename].nil?
+    filename = "/var/www/miq/vmdb/log/automation.log"
+  else
+    filename = options[:filename]
+  end
+  
+  begin
+    file = File.new(filename, "r:UTF-8")
+  rescue => err
+    puts "Error opening log file: #{err}"
+    exit!
+  end
+  
+  if options[:list]
+    list_dumps(file)
+  elsif options[:diff]
+    diff_dumps(file, options[:diff][0], options[:diff][1])
+  else
+    find_dump(file, options[:timestamp])
+  end
+  
+rescue => err
+  puts "#{err}"
+  exit!
+ensure
+  file.close
+end
